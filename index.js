6920390123904c65a5d7a5ee833cb9964ba6fff6
Adrian Majcher <majcherrr87@gmail.com>
2024-01-30 18:10:43 +0100

    the program reads the file and multiplies its last line

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ import { writeFile, readFile, appendFile } from "fs/promises";
 const FILE_NAME = "./data/hello.txt";
 (async () => {
   try {
-    const numberOfFile = Number(await readFile(FILE_NAME, "utf8"));
+    const numberOfFile = await readFile(FILE_NAME, "utf8");
 
-    await writeFile(FILE_NAME, String(numberOfFile * 2), "utf8");
+    const arr = numberOfFile.split("\n");
+
+    const lastElementOfArray = arr[arr.length - 1];
+
+    await appendFile(FILE_NAME, `\n${lastElementOfArray * 2}`, "utf8");
     console.log(`file is saved`);
   } catch (e) {
     console.log(`coś poszło nie tak `, e);
