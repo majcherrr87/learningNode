d41eb7e129504b52f7d836f76af472ae4a129bb7
Adrian Majcher <majcherrr87@gmail.com>
2024-01-30 17:31:14 +0100

    add read file

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import { readFile } from "fs/promises";
-
+import { writeFile, readFile, appendFile } from "fs/promises";
+const FILE_NAME = "./data/hello.txt";
 (async () => {
   try {
-    const data = await readFile("./text.txt", {
-      encoding: "utf8",
-    });
-    console.log(``, data);
-  } catch (error) {
-    console.error(`Bład`, error);
+    const numberOfFile = Number(await readFile(FILE_NAME, "utf8"));
+
+    await writeFile(FILE_NAME, String(numberOfFile * 2), "utf8");
+    console.log(`file is saved`);
+  } catch (e) {
+    console.log(`coś poszło nie tak `, e);
   }
 })();
