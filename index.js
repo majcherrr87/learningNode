5c6887eeef44384a90905690066be658612ba243
Adrian Majcher <majcherrr87@gmail.com>
2024-03-09 16:15:00 +0100

    stream

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import { createReadStream, createWriteStream } from "fs";
 import { pipeline } from "stream/promises";
+import { createGunzip } from "zlib";
 
 (async () => {
-  await pipeline(createReadStream("foto.jpg"), createWriteStream("foto2.jpg"));
+  await pipeline(
+    createReadStream("json2.json"),
+    createGunzip(),
+    createWriteStream("json3.json")
+  );
   console.log("done!");
 })();
