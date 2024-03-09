28ee5a7140f0ba9551e76c7e063b60f2347771f4
Adrian Majcher <majcherrr87@gmail.com>
2024-03-09 16:14:28 +0100

    stream

---
+++ project2/streamdecrypt.js
@@ -0,0 +1,24 @@
+import { pipeline } from "stream/promises";
+import { createReadStream, createWriteStream } from "fs";
+import { createDecipheriv, randomBytes } from "crypto";
+import { promisify } from "util";
+import { scrypt } from "crypto";
+import { ENCRYPTION_SALT } from "../constants.js";
+const proScrypt = promisify(scrypt);
+const randomBytesPromisify = promisify(randomBytes);
+
+const [, , inputFile, outputFile, pwd] = process.argv;
+
+(async () => {
+  const algorytm = "aes-192-cbc";
+  const key = await proScrypt(pwd, ENCRYPTION_SALT, 24);
+  const iv = await randomBytesPromisify(16);
+
+  await pipeline(
+    createReadStream(inputFile),
+    createDecipheriv(algorytm, key, iv),
+    createWriteStream(outputFile)
+  );
+
+  console.log("Done.");
+})();
---
+++ project2/streamencrypt.js
@@ -0,0 +1,24 @@
+import { pipeline } from "stream/promises";
+import { createReadStream, createWriteStream } from "fs";
+import { createCipheriv, randomBytes } from "crypto";
+import { promisify } from "util";
+import { scrypt } from "crypto";
+import { ENCRYPTION_SALT } from "../constants.js";
+const proScrypt = promisify(scrypt);
+const randomBytesPromisify = promisify(randomBytes);
+
+const [, , inputFile, outputFile, pwd] = process.argv;
+
+(async () => {
+  const algorytm = "aes-192-cbc";
+  const key = await proScrypt(pwd, ENCRYPTION_SALT, 24);
+  const iv = await randomBytesPromisify(16);
+
+  await pipeline(
+    createReadStream(inputFile),
+    createCipheriv(algorytm, key, iv),
+    createWriteStream(outputFile)
+  );
+
+  console.log("Done.");
+})();
